c52ced5b7f0c404afa31cabb8316c562e883ac8c
daniel <daniel@LAPTOP-6IG1QGH4>
2018-06-17 15:47:03 -0600

    publicacionproyecto

---
+++ ExamenCoppel/ExamenCoppel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExamenCoppel.MvcApplication" Language="C#" %>
---
+++ ExamenCoppel/ExamenCoppel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExamenCoppel.Startup))]
+namespace ExamenCoppel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
