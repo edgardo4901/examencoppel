0321b811f13271fed1014252bb340162233cdca6
daniel <daniel@LAPTOP-6IG1QGH4>
2018-06-17 19:05:59 -0600

    pantalla de nuevo empleado

--- ExamenCoppel/ExamenCoppel/App_Start/RouteConfig.cs
+++ ExamenCoppel/ExamenCoppel/App_Start/RouteConfig.cs
@@ -16,6 +16,7 @@ namespace ExamenCoppel
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
+                //defaults: new { controller = "Home", action = "Mantenimiento", id = UrlParameter.Optional }
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
--- ExamenCoppel/ExamenCoppel/Startup.cs
+++ ExamenCoppel/ExamenCoppel/Startup.cs
@@ -1,14 +1,117 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Script.Serialization;
 
 [assembly: OwinStartupAttribute(typeof(ExamenCoppel.Startup))]
 namespace ExamenCoppel
 {
-    public partial class Startup
+    public partial class Startup : System.Web.HttpApplication
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            //ConfigureAuth(app);
+            JsonValueProviderFactory jsonValueProviderFactory = null;
+
+            foreach (var factory in ValueProviderFactories.Factories)
+            {
+                if (factory is JsonValueProviderFactory)
+                {
+                    jsonValueProviderFactory = factory as JsonValueProviderFactory;
+                }
+            }
+
+            //remove the default JsonVAlueProviderFactory
+            if (jsonValueProviderFactory != null) ValueProviderFactories.Factories.Remove(jsonValueProviderFactory);
+
+            //add the custom one
+            ValueProviderFactories.Factories.Add(new CustomJsonValueProviderFactory());
+        }
+    }
+    public sealed class CustomJsonValueProviderFactory : ValueProviderFactory
+    {
+
+        private static void AddToBackingStore(Dictionary<string, object> backingStore, string prefix, object value)
+        {
+            IDictionary<string, object> d = value as IDictionary<string, object>;
+            if (d != null)
+            {
+                foreach (KeyValuePair<string, object> entry in d)
+                {
+                    AddToBackingStore(backingStore, MakePropertyKey(prefix, entry.Key), entry.Value);
+                }
+                return;
+            }
+
+            IList l = value as IList;
+            if (l != null)
+            {
+                for (int i = 0; i < l.Count; i++)
+                {
+                    AddToBackingStore(backingStore, MakeArrayKey(prefix, i), l[i]);
+                }
+                return;
+            }
+
+            // primitive
+            backingStore[prefix] = value;
+        }
+
+        private static object GetDeserializedObject(ControllerContext controllerContext)
+        {
+            if (!controllerContext.HttpContext.Request.ContentType.StartsWith("application/json", StringComparison.OrdinalIgnoreCase))
+            {
+                // not JSON request
+                return null;
+            }
+
+            StreamReader reader = new StreamReader(controllerContext.HttpContext.Request.InputStream);
+            string bodyText = reader.ReadToEnd();
+            if (String.IsNullOrEmpty(bodyText))
+            {
+                // no JSON data
+                return null;
+            }
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            serializer.MaxJsonLength = int.MaxValue; //increase MaxJsonLength.  This could be read in from the web.config if you prefer
+            object jsonData = serializer.DeserializeObject(bodyText);
+            return jsonData;
+        }
+
+        public override IValueProvider GetValueProvider(ControllerContext controllerContext)
+        {
+            if (controllerContext == null)
+            {
+                throw new ArgumentNullException("controllerContext");
+            }
+
+            object jsonData = GetDeserializedObject(controllerContext);
+            if (jsonData == null)
+            {
+                return null;
+            }
+
+            Dictionary<string, object> backingStore = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+            AddToBackingStore(backingStore, String.Empty, jsonData);
+            return new DictionaryValueProvider<object>(backingStore, CultureInfo.CurrentCulture);
+        }
+
+        private static string MakeArrayKey(string prefix, int index)
+        {
+            return prefix + "[" + index.ToString(CultureInfo.InvariantCulture) + "]";
+        }
+
+        private static string MakePropertyKey(string prefix, string propertyName)
+        {
+            return (String.IsNullOrEmpty(prefix)) ? propertyName : prefix + "." + propertyName;
         }
     }
 }
